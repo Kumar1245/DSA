c1e533e4d1787f0c96ad54bf9310e206d43ff7c7
Kumar1245 <pankajsingh471999@gmail.com>
2025-09-02 23:51:20 +0530

    valid parenthesis

---
+++ validPrenthsis.js
@@ -0,0 +1,29 @@
+// valid parenthesis strings
+function validParenthesis(s) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
+    for (let char of s) {
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            let last = stack.pop();
+            if (map[last] !== char) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
+
+console.log(validParenthesis("()[]{}")); // true
+console.log(validParenthesis("(]")); // false
+console.log(validParenthesis("([)]")); // false
+console.log(validParenthesis("{[]}")); // true
+console.log(validParenthesis("((()))")); // true
+console.log(validParenthesis("((())")); // false
+console.log(validParenthesis("())")); // false
+    
